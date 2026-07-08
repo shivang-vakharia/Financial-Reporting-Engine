ea8026d258e631910363cd628957641261d1591d
shivang-vakharia <shivang2694@gmail.com>
2026-07-08 15:43:06 +0530

    Fixed a semicolon issue

--- apps/web/src/components/common/Panel.jsx
+++ apps/web/src/components/common/Panel.jsx
@@ -1,14 +1,15 @@
 export default function Panel({ title, actions, children }) {
-    return 
-    <section className="panel">
-        <div className="panel-head">
-            <h2>
-                {title}
-            </h2>
-            
-        {actions}
-        </div>
+    return ( 
+        <section className="panel">
+            <div className="panel-head">
+                <h2>
+                    {title}
+                </h2>
+                
+            {actions}
+            </div>
 
-    {children}
-    </section>;
+        {children}
+        </section>
+    );
 }
